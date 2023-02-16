32827fe0ca6819f7aa292e54d9b0240654bab0f6
Donell <donellmuusha6@gmail.com>
2023-02-16 00:47:55 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main . prints "Programming is like building a multilingual
+ * main - prints "Programming is like building a multilingual
  * puzzle, followed by a newline.
  * Return: Always 0.
  */
@@ -11,6 +11,7 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+
 }
 
 
