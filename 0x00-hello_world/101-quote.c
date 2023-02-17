9856cdc37eca67a361aa57292e1334b997abfdc0
Donell <donellmuusha6@gmail.com>
2023-02-16 17:25:10 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,7 @@
 
  {
 
-         write(2, "and that piece of art is useful\" - Dora Karpar, 2015-10-19\n",59);
+         write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
          return (1);
 
  }	 
