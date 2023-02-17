d107cdfa5e5fb6f8b246550b5c8983edffb78f0c
Donell <donellmuusha6@gmail.com>
2023-02-17 04:38:36 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -20,7 +20,7 @@
 	  printf("Size of a long int: %zu byte(s)\n", (unsigned long)sizeof(c));
 	  printf("Size of a long long int: %zu byte(s)\n",(unsigned long)sizeof(d));
 	  printf("Size of a float: %zu byte(s)\n",sizeof(f));
-	  return (0)
+	  Return (0)
 
  }
 
